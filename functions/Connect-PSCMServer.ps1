e4cb7647ceeac9ee19a4871fa88ae3fece0298a3
luzkenin <luzkenin@gmail.com>
2018-07-05 16:10:28 +0200

    Update Connect-PSCMServer.ps1

--- functions/Connect-PSCMServer.ps1
+++ functions/Connect-PSCMServer.ps1
@@ -10,6 +10,12 @@ function Connect-PSCMServer {
 	SCCM server to connect to
 	.EXAMPLE
 	Connect-PSCMServer -sitecode PRI -ProviderMachineName SCCMServer
+	.NOTES
+	Doesn't actually connect anything really. Just loads the module and creates a PSDrive
+	Also the ConfigurationManager module is a bit finnicky. This function doesn't seem to load it unless it is made discoverable.
+	Even when specifying the path it doesn't seem to load and doesn't stop on error(I guess no error.) You can make the module discoverable by the link in links
+	.LINK
+	https://gallery.technet.microsoft.com/Make-Configuration-Manager-04474a87
 	#>
 	[CmdletBinding()]
 	param (
@@ -24,17 +30,18 @@ function Connect-PSCMServer {
 	
 	process
 	{
-		if($null -eq (Get-Module ConfigurationManager))
+		if(-not(Get-Module ConfigurationManager))
 		{
-			Import-Module (Join-Path $(Split-Path $env:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1) -ErrorAction Stop -Verbose:$VerboseOpt -Force
+			Write-PSFMessage "ConfigurationManager Module was not loaded, loading now" -Level Warning
+			Import-Module ConfigurationManager -ErrorAction Stop #Works when the module is made discoverable
+			#Import-Module (Join-Path $(Split-Path $env:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1) -ErrorAction Stop -Verbose:$VerboseOpt -Force #this doesn't seem to work
 		}
-		if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null)
+		if($null -eq (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue))
 		{
 			New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName -ErrorAction Stop -Verbose:$VerboseOpt
 		}
 
 		Set-Location "$($SiteCode):\" -ErrorAction Stop -Verbose:$VerboseOpt
-		
 	}
 	end
 	{
