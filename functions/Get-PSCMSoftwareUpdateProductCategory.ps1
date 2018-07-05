57768c293bebdc313f6ec6a4b6ce03b43ad61326
luzkenin <luzkenin@gmail.com>
2018-07-05 13:45:33 +0200

    Update Get-PSCMSoftwareUpdateProductCategory.ps1

--- functions/Get-PSCMSoftwareUpdateProductCategory.ps1
+++ functions/Get-PSCMSoftwareUpdateProductCategory.ps1
@@ -1,43 +1,42 @@
 function Get-PSCMSoftwareUpdateProductCategory
 {
-     <#
-	.SYNOPSIS 
-    Get product category
-
-	.DESCRIPTION 
-	Obtain a uniqueID for a software update product category instance. Takes multiple product inputs.
-
-	.PARAMETER IncludeProducts
-	
-    .PARAMETER CIMSessionHash
-    
-	.EXAMPLE 
+	 <#
+	.SYNOPSIS
+	Get product category
+	.DESCRIPTION
+	Get a software update product category instance. Takes multiple product inputs.
+	.PARAMETER IncludeProduct
+	Products you want to include in the search
+	.PARAMETER CIMSessionHash
+	Use Set-PSCMCIMSession to set a variable to CIMSessionHash
+	.EXAMPLE
+	Get-PSCMSoftwareUpdateProductCategory -IncludeProducts "Office 2013","Windows 7","Windows Server 2008 R2", "Windows Server 2012 R2", "Windows Server 2016" -CIMSessionHash $CIMSessionHash
 #>
-    [CmdletBinding()]
-        param (
-        [Parameter(Mandatory, ValueFromPipeline)]
-        $IncludeProducts,
-        [Parameter(Mandatory, ValueFromPipeline)]
-        [hashtable]$CIMSessionHash
-    )
-    
-    begin {
-    }
-    
-    process
-    {
-        $productcategories = Get-CimInstance -ClassName SMS_UpdateCategoryInstance -Filter 'CategoryTypeName="Product"' @CIMSessionHash
-        $FilterForProductCategory = @()
-        foreach($Product in $IncludeProducts)
-        {
-            $FilterForProductCategory += "`$_.LocalizedCategoryInstanceName -eq ""$product"""
-        }
-        $Join = $FilterForProductCategory -join " -or "
-        $ScriptBlock = [scriptblock]::Create( $Join )
-        
-        $productcategories | Where-Object -FilterScript $ScriptBlock
-    }
-    end
-    {
-    }
+	[CmdletBinding()]
+		param (
+		[Parameter(Mandatory, ValueFromPipeline)]
+		$IncludeProduct,
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[hashtable]$CIMSessionHash
+	)
+
+	begin {
+	}
+
+	process
+	{
+		$productcategories = Get-CimInstance -ClassName SMS_UpdateCategoryInstance -Filter 'CategoryTypeName="Product"' @CIMSessionHash
+		$FilterForProductCategory = @()
+		foreach($Product in $IncludeProduct)
+		{
+			$FilterForProductCategory += "`$_.LocalizedCategoryInstanceName -eq ""$product"""
+		}
+		$Join = $FilterForProductCategory -join " -or "
+		$ScriptBlock = [scriptblock]::Create( $Join )
+
+		$productcategories | Where-Object -FilterScript $ScriptBlock
+	}
+	end
+	{
+	}
 }
