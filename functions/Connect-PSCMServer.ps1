159996a15a94f8420832b53f5fb14b13ea7ee1a6
luzkenin <luzkenin@gmail.com>
2018-07-05 12:35:54 +0200

    Update Connect-PSCMServer.ps1

--- functions/Connect-PSCMServer.ps1
+++ functions/Connect-PSCMServer.ps1
@@ -1,29 +1,40 @@
 function Connect-PSCMServer {
+    <#
+    .SYNOPSIS 
+    Connects to SCCM Server and loads PSDrive for CMSite
+	.DESCRIPTION 
+	Connects to SCCM Server and loads PSDrive for CMSite.
+    .PARAMETER SiteCode
+    SiteCode of SCCM site
+    .PARAMETER ProviderMachineName
+    SCCM server to connect to
+    .EXAMPLE
+    Connect-PSCMServer -sitecode PRI -ProviderMachineName SCCMServer
+    #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, ValueFromPipeline)]
-        $SiteCode,
-        [Parameter(Mandatory, ValueFromPipeline)] 
-        $ProviderMachineName,
+        $SiteCode, 
+        $ProviderMachineName = "localhost",
         [switch]$VerboseOpt
     )
     
     begin
     {
-        if((Get-Module ConfigurationManager) -eq $null)
-        {
-            Import-Module (Join-Path $(Split-Path $env:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1) -ErrorAction Stop -Verbose:$VerboseOpt -Force
-        }
     }
     
     process
     {
+        if($null -eq (Get-Module ConfigurationManager))
+        {
+            Import-Module (Join-Path $(Split-Path $env:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1) -ErrorAction Stop -Verbose:$VerboseOpt -Force
+        }
         if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null)
         {
             New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName -ErrorAction Stop -Verbose:$VerboseOpt
         }
 
         Set-Location "$($SiteCode):\" -ErrorAction Stop -Verbose:$VerboseOpt
+        
     }
     end
     {
