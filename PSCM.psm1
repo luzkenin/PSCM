bb4d8c327a487a875818091606fa181b7cdba81c
warrenjr <warrenjr@cassetterunner.io>
2019-02-03 11:53:42 +0100

    Adding structure to module

--- PSCM.psm1
+++ PSCM.psm1
@@ -1,5 +1,35 @@
-# All functions publicly available within the toolset
-foreach ($function in (Get-ChildItem "$PSScriptRoot\functions\*.ps1"))
+[cmdletbinding()]
+param()
+Write-Verbose $PSScriptRoot
+
+Write-Verbose 'Import everything in sub folders folder'
+
+foreach($folder in @('internal', 'functions'))
+
 {
-	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
+
+    $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
+
+    if(Test-Path -Path $root)
+
+    {
+
+        Write-Verbose "processing folder $root"
+
+        $files = Get-ChildItem -Path $root -Filter *.ps1 -Recurse
+
+
+
+        # dot source each file
+
+        $files | where-Object{ $_.name -NotLike '*.Tests.ps1'} | 
+
+            ForEach-Object{Write-Verbose $_.basename; . $_.FullName}
+
+    }
+
 }
+
+
+
+Export-ModuleMember -function (Get-ChildItem -Path "$PSScriptRoot\functions\*.ps1").basename
---
+++ contributing.md
@@ -0,0 +1,10 @@
+# PSCM Contributing.md
+This is the contribute.md of our project PSCM . 
+
+Great to have you here! 
+
+Here are a few ways you can help make this project better.
+
+First - You could help with this page!!
+
+
